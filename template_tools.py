278ae7f0c989410dede0a705f3a27417545791cd
Aviel Boag <avboag@gmail.com>
2022-02-22 02:00:56 +0200

    Performance enhancement.

--- template_tools.py
+++ template_tools.py
@@ -12,8 +12,18 @@ class ParentParent:
     cl: type
 
     def __call__(self, *args, **kwargs):
-        return self.cl(*args, _parent=self, **kwargs)
+        res = object.__new__(self.cl)
+        res.parent = self
+        res.__init__(*args, **kwargs)
 
+        return res
+        # The reason we don't do this:
+        # return self.cl(*args, _parent=self, **kwargs)
+        # is that then _parent would get passed to the __init__
+        # in addition to the proper arguments.
+        # We could wrap __init__ to accomodate this, but
+        # profiling showed the wrapping takes a significant performance
+        # overhead, and this is the more efficient solution.
 
 class DefaultParent(ParentParent):
     def __init__(self, cl, **kwargs):
@@ -61,27 +71,28 @@ def template_new_parent_creator(cls, *args, **kwargs):
     return res
 
 
-def template_new(cls, *args, _parent=None, **kwargs):
-    if _parent is not None:
-        res = object.__new__(cls)
-        res.parent = _parent
+def template_new(cls, *args, _real=False, **kwargs):
+    """
+    If _real=False, create the appropriate parent.
+    If _real=True, create a clean object of this class.
+    Note that this object cannot be used directly as it has no
+    parent. This option is provided solely for pickle.
+    """
 
-        return res
+    if _real:
+        return object.__new__(cls)
 
     return template_new_parent_creator(cls, *args, **kwargs)
 
 
-TEMPLATE_NEW_UNPICKLE_ARGS = ((), {'_parent': False})
+TEMPLATE_NEW_UNPICKLE_ARGS = ((), {'_real': True})
 
 
-def template_getnewargs_ex(self):
+def template_getnewargs_ex(self=None):
     return TEMPLATE_NEW_UNPICKLE_ARGS
 
 
 def template(cl):
-    init = cl.__init__
-    cl.__init__ = lambda *args, _parent, **kwargs: init(*args, **kwargs)
-
     cl.__new__ = template_new
     cl.__getnewargs_ex__ = template_getnewargs_ex
 
